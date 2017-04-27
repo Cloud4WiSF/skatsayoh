b3c8b021cc91601e8ddd3a32faa56311acc80b5c
aesguerra-c4w <aesguerra-c4w@github.com>
2017-04-27 18:28:43 +0800

    Added url to all configs

--- config/auth.js
+++ config/auth.js
@@ -6,7 +6,7 @@ module.exports = {
     'facebookAuth' : {
         'clientID'        : '251840548555589', // your App ID
         'clientSecret'    : 'fda9fd21cb4c4a8a435877239bb67b7b', // your App Secret
-        'callbackURL'     : '/auth/facebook/callback',
+        'callbackURL'     : 'http://skatsayoh.herokuapp.com/auth/facebook/callback',
         'profileURL': 'https://graph.facebook.com/v2.5/me?fields=first_name,last_name,email'
 
     },
@@ -14,13 +14,13 @@ module.exports = {
     'twitterAuth' : {
         'consumerKey'        : 'VmbDcf4SKw1Xv8ssHXmBZ7ed6',
         'consumerSecret'     : 'Q2Landfd0EGlXEA9BphMv56ZkQVg7UFmxrRYwE9bn0cwiXqtgj',
-        'callbackURL'        : '/auth/twitter/callback'
+        'callbackURL'        : 'http://skatsayoh.herokuapp.com/auth/twitter/callback'
     },
 
     'googleAuth' : {
         'clientID'         : 'your-secret-clientID-here',
         'clientSecret'     : 'your-client-secret-here',
-        'callbackURL'      : '/auth/google/callback'
+        'callbackURL'      : 'http://skatsayoh.herokuapp.com/auth/google/callback'
     }
 
 };
