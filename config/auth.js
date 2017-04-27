2a964c16d5396720b35ef99d12f36093dd661c15
aesguerra-c4w <aesguerra-c4w@github.com>
2017-04-27 17:20:41 +0800

    Updated OAuth configurations

--- config/auth.js
+++ config/auth.js
@@ -4,23 +4,23 @@
 module.exports = {
 
     'facebookAuth' : {
-        'clientID'        : 'your-secret-clientID-here', // your App ID
-        'clientSecret'    : 'your-client-secret-here', // your App Secret
-        'callbackURL'     : 'http://localhost:8080/auth/facebook/callback',
+        'clientID'        : '251840548555589', // your App ID
+        'clientSecret'    : 'fda9fd21cb4c4a8a435877239bb67b7b', // your App Secret
+        'callbackURL'     : '/auth/facebook/callback',
         'profileURL': 'https://graph.facebook.com/v2.5/me?fields=first_name,last_name,email'
 
     },
 
     'twitterAuth' : {
-        'consumerKey'        : 'your-consumer-key-here',
-        'consumerSecret'     : 'your-client-secret-here',
-        'callbackURL'        : 'http://localhost:8080/auth/twitter/callback'
+        'consumerKey'        : 'VmbDcf4SKw1Xv8ssHXmBZ7ed6',
+        'consumerSecret'     : 'Q2Landfd0EGlXEA9BphMv56ZkQVg7UFmxrRYwE9bn0cwiXqtgj',
+        'callbackURL'        : '/auth/twitter/callback'
     },
 
     'googleAuth' : {
         'clientID'         : 'your-secret-clientID-here',
         'clientSecret'     : 'your-client-secret-here',
-        'callbackURL'      : 'http://localhost:8080/auth/google/callback'
+        'callbackURL'      : '/auth/google/callback'
     }
 
 };
