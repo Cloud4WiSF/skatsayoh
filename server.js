e541fd008ed75646d84f89736705027ed063462e
aesguerra-c4w <aesguerra-c4w@github.com>
2017-04-27 18:08:22 +0800

    Parse-dashboard

--- server.js
+++ server.js
@@ -22,13 +22,6 @@ mongoose.connect(configDB.url); // connect to our database
 
 require('./config/passport')(passport); // pass passport for configuration
 
-// set up our express application
-app.use(morgan('dev')); // log every request to the console
-app.use(cookieParser()); // read cookies (needed for auth)
-app.use(bodyParser.json()); // get information from html forms
-app.use(bodyParser.urlencoded({ extended: true }));
-
-app.set('view engine', 'ejs'); // set up ejs for templating
 
 var dashboard = new ParseDashboard({
   "apps": [
@@ -50,6 +43,15 @@ var dashboard = new ParseDashboard({
 
 // Dashboard
 app.use('/dashboard', dashboard); // make the Parse Dashboard available at /dashboard
+
+// set up our express application
+app.use(morgan('dev')); // log every request to the console
+app.use(cookieParser()); // read cookies (needed for auth)
+app.use(bodyParser.json()); // get information from html forms
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.set('view engine', 'ejs'); // set up ejs for templating
+
 
 // required for passport
 app.use(session({
